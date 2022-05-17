89b2f65b51024732f616bdcb332d5d6d3461807f
ariya <ariya_watchara@cmu.ac.th>
2022-05-17 13:31:42 +0700

    selection

---
+++ script.js
@@ -0,0 +1,15 @@
+let score=79.4;
+let grade;
+if(score>80){
+    grae = 'A'
+}
+else if (score >= 75) {    
+    grade = 'B+';
+}
+else if(score >= 75){
+    grade = 'B'
+}
+else if (score >= 65) {    
+    grade = 'C+';
+}
+console.log(grade + ' ' + score)
