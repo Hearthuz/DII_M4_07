d3d10399a4a015579d4abdbe03e39ba78bcc7920
ariya <ariya_watchara@cmu.ac.th>
2022-05-18 13:27:17 +0700

    innerHTML

--- script.js
+++ script.js
@@ -4,7 +4,16 @@ function alert1(){
 function alert2(){
     alert('not okay ...')
 }
+
 let ok = document.getElementById('ok');
-ok.addEventListener('click', alert1);
 let cancel = document.getElementById('cancel');
+let showDate = document.getElementById('showDate');
+
+ok.addEventListener('click', alert1);
 cancel.addEventListener('click', alert2);
+showDate.addEventListener('click', function() {
+    document.getElementById('emptySpace').innerHTML = "<b>"+Date()+"</b>";
+});
+showDate.addEventListener('dblclick', function() {
+    document.getElementById('emptySpace').innerHTML = '';
+});
