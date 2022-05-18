57acc0e12f5f376a319d86880764f2b6c0acb748
ariya <ariya_watchara@cmu.ac.th>
2022-05-18 12:19:34 +0700

    event

---
+++ script.js
@@ -0,0 +1,10 @@
+function alert1(){
+    alert('let me see ...')
+}
+function alert2(){
+    alert('not okay ...')
+}
+let ok = document.getElementById('ok');
+ok.addEventListener('click', alert1);
+let cancel = document.getElementById('cancel');
+cancel.addEventListener('click', alert2);
